0866184bf01df5fabcc179d09da0f44a7dad2442
juraevdev <juraevdevpy@gmail.com>
2025-04-18 21:12:44 +0500

    stuff

--- config/settings.py
+++ config/settings.py
@@ -99,7 +99,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleWare',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
