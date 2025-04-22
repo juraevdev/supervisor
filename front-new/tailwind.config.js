b826baa2c015ccdc56cfcb4edde54abc94ed4dda
juraevdev <juraevdevpy@gmail.com>
2025-04-22 22:18:58 +0500

    stuff

--- front-new/tailwind.config.js
+++ front-new/tailwind.config.js
@@ -1,7 +1,11 @@
+// tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'class', // 'media' ham bo‘lishi mumkin, ammo 'class' tavsiya etiladi
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
