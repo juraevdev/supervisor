f41d4c42d366d236f638efd12e11c70f60877b6f
juraevdev <juraevdevpy@gmail.com>
2025-10-31 12:11:23 +0500

    stuff

--- front-new/src/api.js
+++ front-new/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8000"; // Backend API URL
+export const API_BASE_URL = "https://supervisor-a0ki.onrender.com/"; // Backend API URL
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
--- front-new/src/utils/client/apiClient.js
+++ front-new/src/utils/client/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1/todos/",
+  baseURL: "https://supervisor-a0ki.onrender.com/api/v1/todos/",
   headers: {
     "Content-Type": "application/json",
   },
